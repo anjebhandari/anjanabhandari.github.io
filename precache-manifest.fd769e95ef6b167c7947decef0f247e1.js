707a4b9095d98fa8082fa29fd63994add060dacb
Anjana <anjana@united-asian.com>
2020-05-06 16:27:43 +0545

    Updates

---
+++ precache-manifest.fd769e95ef6b167c7947decef0f247e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e0e3691e2cae22466793bce26e469e9",
+    "url": "/anjebhandari/anjanabhandari.github.io/index.html"
+  },
+  {
+    "revision": "3372efd83aa99ea10677",
+    "url": "/anjebhandari/anjanabhandari.github.io/static/css/main.ce09ee66.chunk.css"
+  },
+  {
+    "revision": "6764a06bc02c55fceea8",
+    "url": "/anjebhandari/anjanabhandari.github.io/static/js/2.ebcdef8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/anjebhandari/anjanabhandari.github.io/static/js/2.ebcdef8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3372efd83aa99ea10677",
+    "url": "/anjebhandari/anjanabhandari.github.io/static/js/main.32a24f34.chunk.js"
+  },
+  {
+    "revision": "1adf636361c6aca12d44",
+    "url": "/anjebhandari/anjanabhandari.github.io/static/js/runtime-main.3323366a.js"
+  }
+]);
