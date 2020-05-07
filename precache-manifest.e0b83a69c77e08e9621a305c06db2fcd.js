e1c6b257a52549d760a3bcd3cc656d5c28bddec1
Anjana <anjana@united-asian.com>
2020-05-07 15:33:03 +0545

    Updates

---
+++ precache-manifest.e0b83a69c77e08e9621a305c06db2fcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef2b5072e8bc9cef258c1555b3f41bf9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c7f91c4364baaa821b5",
+    "url": "/static/css/main.ce09ee66.chunk.css"
+  },
+  {
+    "revision": "6764a06bc02c55fceea8",
+    "url": "/static/js/2.ebcdef8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ebcdef8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c7f91c4364baaa821b5",
+    "url": "/static/js/main.361fb8dd.chunk.js"
+  },
+  {
+    "revision": "49bb7587f7f67fd651ac",
+    "url": "/static/js/runtime-main.f9724b17.js"
+  }
+]);
